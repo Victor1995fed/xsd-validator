fdb3e932c320875b759a885b036c5c6d5a6c01cc
Viktor Fedorov <victor1995.fed@yandex.ru>
2020-07-12 18:16:01 +0900

    fix bug

--- resources/views/layouts/header-mobile.blade.php
+++ resources/views/layouts/header-mobile.blade.php
@@ -60,13 +60,33 @@
 </header>
 <div class="sub-header-mobile-2 d-block d-lg-none">
     <div class="header__tool">
+        @guest
+        <div class="header-button-item js-item-menu">
+            <i class="zmdi zmdi-settings"></i>
+            <div class="setting-dropdown js-dropdown">
+                <div class="account-dropdown__body">
+                    <div class="account-dropdown__item">
+                        <a class="nav-link" href="{{ route('login') }}">
+                            <i class="zmdi zmdi-account"></i>Войти</a>
+                    </div>
+                    @if (Route::has('register'))
+                        <div class="account-dropdown__item">
+                            <a class="nav-link" href="{{ route('register') }}">
+                                <i class="zmdi zmdi-settings"></i>Зарегистрироваться</a>
+                        </div>
+
+                    @endif
+                </div>
+            </div>
+        </div>
+        @else
         <div class="account-wrap">
             <div class="account-item account-item--style2 clearfix js-item-menu">
                 <div class="image">
                     <img src="{{asset('images/icon/ava-v1.png')}}" alt="John Doe" />
                 </div>
                 <div class="content">
-                    <a class="js-acc-btn" href="#">{{Auth::user()->name}}</a>
+{{--                    <a class="js-acc-btn" href="#">{{Auth::user()->name}}</a>--}}
                 </div>
                 <div class="account-dropdown js-dropdown">
                     <div class="account-dropdown__body">
@@ -91,7 +111,9 @@
                         </form>
                     </div>
                 </div>
+
             </div>
         </div>
+        @endguest
     </div>
 </div>
