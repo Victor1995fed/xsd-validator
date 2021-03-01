c2c1fcc42d5c8059afabdebb83010f1a983406bd
Viktor Fedorov <victor1995.fed@yandex.ru>
2021-03-01 16:43:58 +0900

    add image

--- resources/views/layouts/footer.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="row">
-    <div class="col-md-12">
-        <div class="copyright">
-            <p>Copyright © 2020 Colorlib. Все права защищены. Использовался шаблон от <a href="https://colorlib.com">Colorlib</a>.</p>
-        </div>
-    </div>
-</div>
